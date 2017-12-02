180a8b757d720b4b6b4276f50131dd43ad53e92a
Cathy Huang <cathy@c1exchange.com>
2017-12-01 18:18:19 -0800

    update test cases

--- test/spec/modules/c1xBidAdapter_spec.js
+++ test/spec/modules/c1xBidAdapter_spec.js
@@ -17,30 +17,23 @@ describe('C1XAdapter', () => {
     let bid = {
       'bidder': 'c1x',
       'adUnitCode': 'adunit-code',
-      'sizes': [[300, 250], [300, 600]]
+      'sizes': [[300, 250], [300, 600]],
+      'params': {
+        'siteId': '9999'
+      }
     };
 
-    it('should return false when required params are not passed', () => {
-      let bid = Object.assign({}, bid);
-      global.window.c1x_pubtag.siteId = '';
-      console.log(global.window.location); // TO DO: check how to add attr to window obj
-      expect(c1xAdapter.isBidRequestValid(bid)).to.equal(false);
+    it('should return true when required params are passed', () => {
+      expect(c1xAdapter.isBidRequestValid(bid)).to.equal(true);
     });
-  });
 
-  describe('buildRequests', () => {
-    let bidRequests = [
-      {
-        'bidder': 'c1x',
-        'adUnitCode': 'adunit-code',
-        'sizes': [[300, 250], [300, 600]],
-      }
-    ];
-
-    it('sends bid request to ENDPOINT via GET', () => {
-      const request = c1xAdapter.buildRequests(bidRequests);
-      expect(request.url).to.equal(ENDPOINT);
-      expect(request.method).to.equal('GET');
+    it('should return false when required params are not found', () => {
+      let bid = Object.assign({}, bid);
+      delete bid.params;
+      bid.params = {
+        'siteId': null
+      };
+      expect(c1xAdapter.isBidRequestValid(bid)).to.equal(false);
     });
   });
 });
